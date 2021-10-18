b3963dcde8a909b43c42e02afc083681699efde8
Muhamad Airi <codenameaeri@gmail.com>
2021-10-19 00:26:26 +0700

    Add splash screen & color style

--- app/src/main/java/com/example/awesomeapp/MainActivity.kt
+++ app/src/main/java/com/example/awesomeapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.awesomeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        activityScope.launch {
+            delay(4000L)
+            val moveListActivity = Intent(this@MainActivity, ListActivity::class.java)
+            startActivity(moveListActivity)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activityScope.cancel()
     }
 }
